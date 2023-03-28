36b4ff9f27200911a2991facd70ec9e7db06f192
fregat17 <gavrikovalexey1@gmail.com>
2023-03-28 17:40:41 +0300

    works, no message input

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import operator
 from os import environ
+from random import choice
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.enums import ParseMode
@@ -48,30 +49,19 @@ async def get_data(dialog_manager: DialogManager, air_connect, **kwargs):
     flags = dialog_manager.dialog_data.get("chosen_flags")
     data = air_connect.get_fields(["Hieroglyph", "Pinyin", "Meaning", "Haohan", "TrainCh"])
     cards = make_cards(data)
-    ready_template = render_spoilers(flags)
+    card = choice(cards)
 
     return {
-        "ready_template": ready_template,
-        "card": cards[23]
+        "flags": flags,
+        "card": card
     }
+
 
 async def go_anneal(c, dialog, manager: DialogManager):  # , air_connect: AirtableActions
     checked_flags = manager.find("regimes_kbd")
     manager.dialog_data["chosen_flags"] = checked_flags.get_checked()
 
-    await c.answer("asd")
     await manager.next()
-    #word_records = air_connect.get_fields(["Hieroglyph", "Pinyin", "Meaning", "Haohan", "TrainCh"])
-    #print(word_records[0:2])
-
-
-    # cards = make_cards(word_records[0:2])
-    # print(cards)
-    # test = ReadyMsg(cards[1], target="hieroglyph").render()
-    #
-    # manager.current_context().dialog_data["age"] = cards
-    #
-    # await m.answer(test, parse_mode=ParseMode.MARKDOWN_V2, disable_web_page_preview=True)
 
 
 regimes_kbd = Multiselect(
@@ -85,29 +75,19 @@ regimes_kbd = Multiselect(
 )
 
 get_regime = Button(Const("Process"), id="get_regime", on_click=go_anneal)
+next_card = Button(Const("Next"), id="next")
 
+card_template = Jinja("""
+{% macro render_field(field) %}
+    {% if field in flags %}<tg-spoiler>{{card[field]}}</tg-spoiler>{% else %}<b>{{card[field]}}</b>{% endif %}
+{% endmacro %}
 
-def render_spoilers(flags):
-    template = """
-<b>{{card.hieroglyph}}</b>
-<b>{{card.pinyin}}</b>
-<b>{{card.meaning}}</b>
+{% for field in ['hieroglyph', 'pinyin', 'meaning'] %}
+{{ render_field(field) }}
+{% endfor %}
 
 <a href="{{card.haohan}}">haohan</a>|<a href="{{card.trainch}}">trainch</a>
-"""
-
-    if "hieroglyph" in flags:
-        template = template.replace("<b>{{card.hieroglyph}}</b>", "<span class=\"tg-spoiler\">{{card.hieroglyph}}</span>")
-    if "pinyin" in flags:
-        template = template.replace("<b>{{card.pinyin}}</b>", "<span class=\"tg-spoiler\">{{card.pinyin}}</span>")
-    if "meaning" in flags:
-        template = template.replace("<b>{{card.meaning}}</b>", "<span class=\"tg-spoiler\">{{card.meaning}}</span>")
-
-    return template
-
-
-
-#spoiler
+""")
 
 dialog = Dialog(
     Window(
@@ -118,44 +98,29 @@ dialog = Dialog(
         getter=get_flags
     ),
     Window(
-        Jinja(),
+        card_template,
+        next_card,
         parse_mode=ParseMode.HTML,
         state=DialogSG.learning,
         getter=get_data,
-        disable_web_page_preview=True
+        disable_web_page_preview=True,
     ),
 )
 
 
-# def main():
-#     bot = Bot(token=BOT_TOKEN)
-#     dp = Dispatcher(storage=MemoryStorage())
-#
-#     dp.message.middleware(AirtableMiddleware(AirtableActions(AIRTABLE_TOKEN, BASE, "Слова")))
-#     dp.message.register(go_anneal)
-#
-#     dp.run_polling(bot)
-# if __name__ == "__main__":
-#     main()
-
-def new_registry():
-    registry = DialogRegistry()
-    registry.register(dialog)
-    return registry
-
-
 async def main():
-    # real main
     bot = Bot(token=BOT_TOKEN)
-
     storage = MemoryStorage()
     dp = Dispatcher(storage=storage)
+
     dp.callback_query.middleware(AirtableMiddleware(AirtableActions(AIRTABLE_TOKEN, BASE, "Слова")))
+    dp.message.middleware(AirtableMiddleware(AirtableActions(AIRTABLE_TOKEN, BASE, "Слова")))
+
     dp.message.register(start, F.text == "/start")
 
-    registry = new_registry()
+    registry = DialogRegistry()
+    registry.register(dialog)
     registry.setup_dp(dp)
-
     await dp.start_polling(bot)
 
 
